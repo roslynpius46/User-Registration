5af417ca0e170752d905dbefc2412b024cc03df1
roslynpius <roslyn.pius@gmail.com>
2023-12-03 20:54:48 +0530

    Added: UC 1- Validating first name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -1,8 +1,32 @@
 package com.bridgelabz.userregistration;
+import java.util.Scanner;
 
 public class UserRegistration {
     public static void main(String[] args)
     {
-        System.out.println("User Registration Implementation");
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt user for input
+        System.out.println("Enter a valid first name:");
+        String firstName = sc.nextLine();
+
+        // Validate the first name
+        if (isValidFirstName(firstName)) {
+            System.out.println("Valid first name: " + firstName);
+        } else {
+            System.out.println("Invalid first name. Please make sure it starts with a capital letter and has a minimum length of 3 characters.");
+        }
+
+        sc.close();
+    }
+
+    /**
+     * @desc Checking if first name is valid
+     * @param firstName first name input by user
+     * @return True if valid else False
+     */
+    private static boolean isValidFirstName(String firstName) {
+        // Check if the first name starts with a capital letter and has a minimum length of 3 characters
+        return firstName.matches("[A-Z][a-zA-Z]{2,}");
     }
 }
