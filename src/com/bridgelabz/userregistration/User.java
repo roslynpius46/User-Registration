d80f34aa65035869e76c60a7a192e4fade6811f3
roslynpius <roslyn.pius@gmail.com>
2023-12-03 22:26:38 +0530

    Added: UC 5- Validating Password Rule 1

--- src/com/bridgelabz/userregistration/User.java
+++ src/com/bridgelabz/userregistration/User.java
@@ -8,6 +8,7 @@ public class User {
     private String lastName;
     private String email;
     private String phoneNo;
+    private String password;
 
     /**
      * @desc Constructor that initializes user details
@@ -15,42 +16,61 @@ public class User {
      * @param lastName last name of user
      * @param email Email ID of user
      * @param phoneNo Phone No. of user
+     * @param password user password
+     *
      */
-    public User(String firstName,String lastName,String email,String phoneNo)
+    public User(String firstName,String lastName,String email,String phoneNo, String password )
     {
         this.firstName=firstName;
         this.lastName=lastName;
         this.email=email;
         this.phoneNo=phoneNo;
+        this.password=password;
+
     }
 
     /**
-     * @desc Checking if first or last name is valid
-     * @param name first or last name input by user
+     * @desc Checking if first is valid
      * @return True if valid else False
      */
-    public boolean isValidName(String name) {
+    public boolean isValidFirstName() {
         // Check if name starts with a capital letter and has a minimum length of 3 characters
-        return name.matches("[A-Z][a-zA-Z]{2,}");
+        return firstName.matches("[A-Z][a-zA-Z]{2,}");
+    }
+
+    /**
+     * @desc Checking if last name is valid
+     * @return True if valid else False
+     */
+    public boolean isValidLastName() {
+        // Check if name starts with a capital letter and has a minimum length of 3 characters
+        return lastName.matches("[A-Z][a-zA-Z]{2,}");
     }
 
     /**
      * @desc Checking if email ID is valid
-     * @param email email ID input by user
      * @return True if valid else False
      */
-    public boolean isValidEmailID(String email) {
+    public boolean isValidEmailID() {
         // Check if the email follows the specified format
         return email.matches("[a-zA-Z]+\\.[a-zA-Z]+@[a-zA-Z]+\\.[a-zA-Z]+(\\.[a-zA-Z]+)?");
     }
 
     /**
      * @desc Checking if phone number is valid
-     * @param phoneNo phone number input by user
      * @return True if valid else False
      */
-    public boolean isValidPhoneNo(String phoneNo) {
+    public boolean isValidPhoneNo() {
         // Check if the email follows the specified format
         return phoneNo.matches("91 \\d{10}");
+    }
+
+    /**
+     * @desc Checking if password is valid
+     * @return True if valid else False
+     */
+    public boolean isValidPassword() {
+        // Rule 1: min 8 characters
+        return password.length() >= 8;
     }
 }
--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -15,12 +15,14 @@ public class UserRegistration {
         String email = sc.nextLine();
         System.out.println("Enter a valid Phone No.:");
         String phoneNo= sc.nextLine();
+        System.out.println("Enter a valid Password.:");
+        String password= sc.nextLine();
 
 
-        User u1=new User(firstName,lastName,email,phoneNo);
+        User u1=new User(firstName,lastName,email,phoneNo,password);
 
         // Validate the first name
-        if (u1.isValidName(firstName)) {
+        if (u1.isValidFirstName()) {
             System.out.println("Valid first name: " + firstName);
         }
         else {
@@ -28,7 +30,7 @@ public class UserRegistration {
         }
 
         // Validate the last name
-        if (u1.isValidName(lastName)) {
+        if (u1.isValidLastName()) {
             System.out.println("Valid last name: " + lastName);
         }
         else {
@@ -36,7 +38,7 @@ public class UserRegistration {
         }
 
         // Validate the email ID
-        if (u1.isValidEmailID(email)) {
+        if (u1.isValidEmailID()) {
             System.out.println("Valid email: " + email);
         }
         else {
@@ -44,12 +46,22 @@ public class UserRegistration {
         }
 
         // Validate the phone number
-        if (u1.isValidPhoneNo(phoneNo)) {
+        if (u1.isValidPhoneNo()) {
             System.out.println("Valid Phone No.: " + phoneNo);
         }
         else {
             System.out.println("Invalid Phone No.");
         }
+
+        // Validate the password
+        if (u1.isValidPassword()) {
+            System.out.println("Valid Password");
+        }
+        else {
+            System.out.println("Invalid Password");
+        }
+
+
 
         sc.close();
     }
